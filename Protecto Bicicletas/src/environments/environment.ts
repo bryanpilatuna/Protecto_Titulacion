b679420efbbabbc627a47a7698a2b6d72726fa1d
Bryan España <byips1995@gmail.com>
2020-07-17 11:06:17 -0500

    Resubir el proyecto

---
+++ Protecto Bicicletas/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAdvTmX8tFj1saHU-E-JDrn4-NGPcZ3v4M",
+    authDomain: "proyecto-bicicleta.firebaseapp.com",
+    databaseURL: "https://proyecto-bicicleta.firebaseio.com",
+    projectId: "proyecto-bicicleta",
+    storageBucket: "proyecto-bicicleta.appspot.com",
+    messagingSenderId: "601256385579",
+    appId: "1:601256385579:web:d21ce7799d582273e51e6e",
+    measurementId: "G-GWNW7KM28S"
+  };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
