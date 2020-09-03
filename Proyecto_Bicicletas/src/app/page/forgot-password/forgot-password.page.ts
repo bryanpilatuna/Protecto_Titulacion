d712d66268c0b5522e8d0f18827291006027c791
Bryan España <byips1995@gmail.com>
2020-09-03 13:52:15 -0500

    validacion olvidar contraseña y estilos para el login

--- Proyecto_Bicicletas/src/app/page/forgot-password/forgot-password.page.ts
+++ Proyecto_Bicicletas/src/app/page/forgot-password/forgot-password.page.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-
-  constructor(private authSvc: AuthService, private router: Router) { }
+  formGroup: FormGroup; 
+  constructor(private authSvc: AuthService, private router: Router,public formBuilder: FormBuilder) {
+    this.crearvalidaciones();
+  }
 
   ngOnInit() {
   }
+
+  crearvalidaciones(){
+    // Campo Contraseña
+    const emailControl = new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email,
+        Validators.minLength(10),
+        Validators.maxLength(40)
+
+    ]));
+    // Añado Propiedades al Form
+    this.formGroup = this.formBuilder.group({emailControl });
+  }
+
   async onResetPassword(email) {
     try {
       await this.authSvc.resetPassword(email.value);
