ae69f41b244c0f4adba8dfceb236fbe2cdb08cb1
Bryan España <byips1995@gmail.com>
2020-11-06 02:31:55 -0500

    edicicones sin restriccion

--- Proyecto_Bicicletas_Web/src/app/page/edit-cliente/edit-cliente.page.ts
+++ Proyecto_Bicicletas_Web/src/app/page/edit-cliente/edit-cliente.page.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NavController, LoadingController } from '@ionic/angular';
-import { Tienda } from '../../model/tienda.interface';
-import {TiendaService} from '../../services/tienda.service';
+import { DatosUsuario } from '../../model/user.interface';
+import { AdministradorService } from '../../services/administrador.service';
 @Component({
   selector: 'app-edit-cliente',
   templateUrl: './edit-cliente.page.html',
   styleUrls: ['./edit-cliente.page.scss'],
 })
 export class EditClientePage implements OnInit {
-
-  constructor() { }
+  user:DatosUsuario;
+  iduser= null;
+  constructor(
+    private route: ActivatedRoute,
+    private nav: NavController, 
+    private Service:AdministradorService,
+     private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
+    this.iduser = this.route.snapshot.params['id'];
+    if (this.iduser){
+      this.loadTodo();
+    }
+  }
+
+  async loadTodo(){
+    const loading = await this.loadingController.create({
+      message: 'Loading....'
+    });
+    await loading.present();
+
+    this.Service.getUsuario(this.iduser).subscribe(usuario => {
+      loading.dismiss();
+      console.log(usuario);
+      this.user = usuario;
+    });
+  }
+
+  async saveTodo() {
+    const loading = await this.loadingController.create({
+      message: 'Saving....'
+    });
+    await loading.present();
+ 
+    if (this.iduser) {
+      this.Service.updateUsuario(this.user, this.iduser).then(() => {
+        loading.dismiss();
+        this.nav.navigateForward('/cliente-administrador');
+      });
+    } 
   }
 
 }
