c6f1328f892c9bccd7aa7748caf1a021b1ffd119
Bryan España <byips1995@gmail.com>
2020-11-14 16:06:50 -0500

    estilos

--- Proyecto_Bicicletas/src/app/page/formulario-donacion/formulario-donacion.page.ts
+++ Proyecto_Bicicletas/src/app/page/formulario-donacion/formulario-donacion.page.ts
@@ -95,7 +95,7 @@ export class FormularioDonacionPage implements OnInit {
     const descripcionControl = new FormControl('', Validators.compose([
       Validators.required,
       Validators.minLength(5),
-      Validators.maxLength(30)
+      Validators.maxLength(50)
     ]));
     
     this.formGroup = this.formBuilder.group({fechaControl,tiendaControl,estadoControl,descripcionControl });
