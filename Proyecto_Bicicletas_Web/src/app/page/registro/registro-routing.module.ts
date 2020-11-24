c2ea5e4c0b166aec1d30e13f22cec753c22cdcd4
Bryan España <byips1995@gmail.com>
2020-11-23 21:15:22 -0500

    login estilos

--- Proyecto_Bicicletas_Web/src/app/page/registro/registro-routing.module.ts
+++ Proyecto_Bicicletas_Web/src/app/page/registro/registro-routing.module.ts
@@ -3,23 +3,36 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RegistroPage } from './registro.page';
 
+
+
 const routes: Routes = [
   {
-    path: '',
+    path: 'registro',
     component: RegistroPage,
     children: [
-    {
-      path:'tienda',
-      loadChildren: () => import('../registro-tienda/registro-tienda.module').then( m => m.RegistroTiendaPageModule)      
-    },
-    {
-      path:'cliente',
-      loadChildren: () => import('../registro-cliente/registro-cliente.module').then( m => m.RegistroClientePageModule)
-      
-    }
-
+      {
+        path:'tienda',
+        loadChildren: () => import('../registro-tienda/registro-tienda.module').then( m => m.RegistroTiendaPageModule)      
+      },
+      {
+        path:'cliente',
+        loadChildren: () => import('../registro-cliente/registro-cliente.module').then( m => m.RegistroClientePageModule)
+        
+      },
+      {
+        path: '',
+        redirectTo: 'registro/cliente',
+        pathMatch: 'full'
+      }
     ]
+  },
+  
+  {
+    path: '',
+    redirectTo: 'registro/cliente',
+    pathMatch: 'full'
   }
+  
 ];
 
 @NgModule({
