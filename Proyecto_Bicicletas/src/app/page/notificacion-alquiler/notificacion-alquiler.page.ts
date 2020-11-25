3276b3fd6c7e21f5b70d6ce601c18648d2e3ed90
Bryan España <byips1995@gmail.com>
2020-11-25 16:20:19 -0500

    finalizar

--- Proyecto_Bicicletas/src/app/page/notificacion-alquiler/notificacion-alquiler.page.ts
+++ Proyecto_Bicicletas/src/app/page/notificacion-alquiler/notificacion-alquiler.page.ts
@@ -12,6 +12,7 @@ import { DonacionService } from 'src/app/service/donacion.service';
 })
 export class NotificacionAlquilerPage implements OnInit {
   notificaciones: Notificaciones[];
+  notificaciones2: Notificaciones[];
   id: any;
   tiendas:  datosTiendas[];
   constructor(
@@ -25,6 +26,7 @@ export class NotificacionAlquilerPage implements OnInit {
       this.notificaciones = notificaciones;
       
       })
+
 
       this.Servicio.getTiendas().subscribe((tiendas) =>{
         
