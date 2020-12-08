f1cb4f7967d598d77e81d844295d5e12ffa57d21
Bryan Pilatuna <bryan_Adri_94@outlook.com>
2020-12-07 20:50:16 -0500

    inicio

--- Proyecto_Bicicletas_Web/src/app/page/registro-cliente/registro-cliente.page.ts
+++ Proyecto_Bicicletas_Web/src/app/page/registro-cliente/registro-cliente.page.ts
@@ -25,7 +25,6 @@ export class RegistroClientePage implements OnInit {
     }
 
   ngOnInit() {
-    this.pintarpestaña();
 
     
   }
@@ -136,28 +135,6 @@ export class RegistroClientePage implements OnInit {
     this.passwordTypeInput = this.passwordTypeInput === 'text' ? 'password' : 'text';
   }
 
-  pintarpestaña(){
-
-    /// Url actual
-let url = window.location.href;
-
-/// Elementos de li
-const tabs = ["home", "mapa", "registro-cliente", "registro-tienda", "descagar-app"];
-
-tabs.forEach(e => {
-    /// Agregar .php y ver si lo contiene en la url
-    if (url.indexOf(e) !== -1) {
-        /// Agregar tab- para hacer que coincida la Id
-        setActive("tab-" + e);
-    }
-
-});
-
-/// Funcion que asigna la clase active
-function setActive(id) {
-    document.getElementById(id).setAttribute("class", "nav-item active");
-}
-
-  }
+ 
 
 }
