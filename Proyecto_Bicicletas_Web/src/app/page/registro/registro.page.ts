340ad19b77fe89703c09b7bf1af4d6bcdc7d7e6e
Bryan Pilatuna <bryan_Adri_94@outlook.com>
2021-01-17 18:46:04 -0500

    Correccion Ing. Ivonne

--- Proyecto_Bicicletas_Web/src/app/page/registro/registro.page.ts
+++ Proyecto_Bicicletas_Web/src/app/page/registro/registro.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -9,7 +9,7 @@ import { AuthService } from '../../services/auth.service';
 export class RegistroPage implements OnInit {
  
 
-  constructor(private authSvc: AuthService) {
+  constructor(private authSvc: AuthService,private alertCtrl: AlertController) {
   
     
      }
@@ -20,6 +20,17 @@ export class RegistroPage implements OnInit {
 
   }
 
+
+  async mensajeubi() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Atención',
+      message: 'Activa la ubicación para registrar una tienda.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 
   
